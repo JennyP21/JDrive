09240d3a90072fb593fd87966e11ae41d596b704
Jenny Patel <pateljenny555@gmail.com>
2023-04-20 13:43:18 -0700

    Adding useItemVisibilityHook to Advanced search menu

--- src/components/Navbar/Container/Navbar.tsx
+++ src/components/Navbar/Container/Navbar.tsx
@@ -4,9 +4,12 @@ import Logo from "../Content/Logo/Logo";
 import Elements from "../Content/Elements/Elements";
 import AdvancedSearch from "../Content/Searchbar/AdvancedSearch";
 import Search from "../Content/Searchbar/Search";
+import useItemVisibility from "../../hooks/useItemVisibility";
 
 const Navbar = () => {
   const [adSearchVisibility, setAdSearchVisibility] = useState(false);
+
+  useItemVisibility({ itemVisibility: adSearchVisibility, setItemVisibility: setAdSearchVisibility, containerClassName: "search-menu-icon", contentClassname: "adsearch" });
 
   return (
     <>
